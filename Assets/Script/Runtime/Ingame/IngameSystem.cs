165f05fa11a68426b6fc194dad9d214f16dba5bc
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-25 11:21:39 +0900

    [add]インゲームのシステムを追加

---
+++ Assets/Script/Runtime/Ingame/IngameSystem.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+public class IngameSystem : MonoBehaviour
+{
+    private PhaseKind _nowPhase;
+    
+    private GameObject _cucumber;
+    
+    private void Awake()
+    {
+        _nowPhase = PhaseKind.Phase1;
+    }
+
+    public void SetCucumberInstance(GameObject instance)
+    {
+        if (!instance)
+        {
+            Debug.LogWarning("対象のインスタンスがありません");
+        }
+        
+        _cucumber = instance;
+    }
+
+
+
+
+    /// <summary>
+    /// 次のフェーズに遷移する
+    /// </summary>
+    /// <param name="phase"></param>
+    public void NextPhaseEvent()
+    {
+        
+    }
+
+    public enum PhaseKind
+    {
+        None = 0,
+        Phase1 = 1,
+        Phase2 = 2,
+        Phase3 = 3,
+        Result = 4,
+    }
+}
