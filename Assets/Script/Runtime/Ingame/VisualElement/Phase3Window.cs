6b8621253e242d089afdbb5b84e4873c039bb20a
Oriku0308 <O.riku8863@gmail.com>
2025-02-25 23:22:59 +0900

    <Add> フェーズ3の演出以外の要素が完成 あとは演出を追加するのとスコアの計算を組み込むだけ

---
+++ Assets/Script/Runtime/Ingame/VisualElement/Phase3Window.cs
@@ -0,0 +1,22 @@
+﻿using SymphonyFrameWork.Utility;
+using System.Threading.Tasks;
+using UnityEngine.UIElements;
+
+[UxmlElement]
+public partial class Phase3Window : SymphonyVisualElement
+{
+    private Button _chargeButton;
+    public Button ChargeButton {  get => _chargeButton;  }
+    
+    //ゲージのゲージの実装ができていない
+
+    public Phase3Window() : base("UITK/Ingame/Phase3Window") { }
+
+    protected override Task Initialize_S(TemplateContainer container)
+    {
+        _chargeButton = container.Q<Button>("ChargeButton");
+
+        return Task.CompletedTask;
+    }
+
+}
