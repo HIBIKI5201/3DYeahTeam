8fff1244bd9cd775a6e44aaf116a5a93e2d962a2
Oriku0308 <O.riku8863@gmail.com>
2025-02-25 12:07:00 +0900

    <Add> チャージ用のManager一旦完成 PushCounterからボタンを押された回数を受け取ることができる

---
+++ Assets/Script/Runtime/Ingame/ChargeManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ChargeManager : MonoBehaviour
+{
+    //チャージ機能を作る
+    //時間制限があり、そのなかでボタンを押された回数を保存しておく
+    private float _pushCounter;
+    public float PushCounter { get => _pushCounter; }
+    private float _timer;
+
+    [SerializeField]
+    private float _timLimit;
+
+    private void Start()
+    {
+        _timer = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time < _timLimit + _timer)
+        {
+            ChargeAction();
+        }
+    }
+
+    private void ChargeAction()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _pushCounter++;
+            Debug.Log($"現在値は　{_pushCounter}");
+        }
+    }
+}
