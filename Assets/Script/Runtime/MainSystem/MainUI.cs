c3eca3e484102c98163a9f5cc0027803adf5f13a
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-25 13:29:18 +0900

    [fix]初期化でフェードを消すように

--- Assets/Script/Runtime/MainSystem/MainUI.cs
+++ Assets/Script/Runtime/MainSystem/MainUI.cs
@@ -14,6 +14,11 @@ public class MainUI : MonoBehaviour
         _blackScreen = _uiDocument.rootVisualElement.Q<VisualElement>("black-screen");
     }
     
+    private void Start()
+    {
+        _blackScreen.style.opacity = 0;
+    }
+    
     public async Task FadeIn(float timer) => await UI_Utility.FadeIn(_blackScreen, timer);
     public async Task FadeOut(float timer) => await UI_Utility.FadeOut(_blackScreen, timer);
 }
