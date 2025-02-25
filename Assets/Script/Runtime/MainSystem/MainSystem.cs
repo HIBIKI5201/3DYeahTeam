b641262a17e9c55d67fbc05c5a78d51fa753c826
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-25 13:28:53 +0900

    [update]シーンのロードシステムを実装

--- Assets/Script/Runtime/MainSystem/MainSystem.cs
+++ Assets/Script/Runtime/MainSystem/MainSystem.cs
@@ -1,16 +1,60 @@
+using System;
 using UnityEngine;
+using System.Threading.Tasks;
+using SymphonyFrameWork.System;
+using UnityEngine.SceneManagement;
 
 public class MainSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private MainUI _mainUI;
+
+    private Task _loadTask = Task.CompletedTask;
+
+    private SceneListEnum _nowScene;
+    
+    #if UNITY_EDITOR
+    [SerializeField] private SceneListEnum _targetScene;
+    
+    [ContextMenu("SceneChange")]
+    private void DebugSceneChange() => SceneChange(_targetScene);
+    #endif
+    private void Awake()
     {
+        _mainUI = GetComponent<MainUI>();
         
+        //現在のシーンを保存する
+        Scene scene = SceneManager.GetActiveScene();
+        if (!Enum.TryParse(scene.name, out _nowScene))
+        {
+            Debug.LogWarning($"Scene '{scene.name}' is not a valid scene.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public async void SceneChange(SceneListEnum scene)
     {
-        
+        if (!_loadTask.IsCompleted)
+        {
+            Debug.LogWarning("既にロードが開始されています");
+            return;
+        }
+
+        _loadTask = LoadScene();
+
+        await _loadTask;
+
+        async Task LoadScene()
+        {
+            await _mainUI.FadeOut(1f);
+
+            // 今のシーンをアンロード
+            await SceneLoader.UnloadScene(_nowScene.ToString());
+
+            // 次のシーンをロードする
+            await SceneLoader.LoadScene(scene.ToString());
+            _nowScene = scene;
+            SceneLoader.SetActiveScene(scene.ToString());
+
+            await _mainUI.FadeIn(1f);
+        }
     }
 }
