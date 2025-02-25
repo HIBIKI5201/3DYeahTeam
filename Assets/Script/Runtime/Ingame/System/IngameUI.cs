c2a39987505421c1155e4735c4e99b5e59332025
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-25 12:35:21 +0900

    [update]UI用のUtilityクラスを実装

--- Assets/Script/Runtime/Ingame/System/IngameUI.cs
+++ Assets/Script/Runtime/Ingame/System/IngameUI.cs
@@ -15,37 +15,6 @@ public class IngameUI : MonoBehaviour
         _blackScreen = _uiDocument.rootVisualElement.Q<VisualElement>("black-screen");
     }
     
-    public async Task FadeIn(float timer)
-    {
-        float speed = 1 / timer; //透明度の秒間変化スピード
-        
-        do
-        {
-            //透明度を変化させる
-            _blackScreen.style.opacity = _blackScreen.style.opacity.value
-                                         - speed * Time.deltaTime;
-            
-            await Awaitable.NextFrameAsync();
-            timer -= Time.deltaTime;
-        } while (timer > 0);
-
-        _blackScreen.style.opacity = 0;
-    }
-    
-   public async Task FadeOut(float timer)
-    {
-        float speed = 1 / timer; //透明度の秒間変化スピード
-        
-        do
-        {
-            //透明度を変化させる
-            _blackScreen.style.opacity = _blackScreen.style.opacity.value
-                                         + speed * Time.deltaTime;
-            
-            await Awaitable.NextFrameAsync();
-            timer -= Time.deltaTime;
-        } while (timer > 0);
-        
-        _blackScreen.style.opacity = 1;
-    }
+    public async Task FadeIn(float timer) => await UI_Utility.FadeIn(_blackScreen, timer);
+    public async Task FadeOut(float timer) => await UI_Utility.FadeOut(_blackScreen, timer);
 }
