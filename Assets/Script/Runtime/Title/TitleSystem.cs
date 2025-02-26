969ac5a8f10956ddc236dee008da8f680ca176f9
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-26 21:28:15 +0900

    [update]タイトルシーンで宇宙船シーンをロード

--- Assets/Script/Runtime/Title/TitleSystem.cs
+++ Assets/Script/Runtime/Title/TitleSystem.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 
 public class TitleSystem : MonoBehaviour
 {
+    private void Awake()
+    {
+        _ = SceneLoader.LoadScene(SceneListEnum.SpaceShip.ToString());
+    }
+
     void Start()
     {
         var audio = ServiceLocator.GetInstance<AudioManager>();
