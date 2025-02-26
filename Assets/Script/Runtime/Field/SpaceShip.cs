4d46dbc27badfcf1526f7f25c516a7c792e15d4d
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-26 22:21:08 +0900

    [add]宇宙船のマネージャーを追加

---
+++ Assets/Script/Runtime/Field/SpaceShip.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{
+    [SerializeField]
+    private CucumberManager _cucumber;
+    public CucumberManager Cucumber { get => _cucumber; }
+}
