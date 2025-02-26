3e03d2a546997df0fdc2a56e8b07dd9121a0abd8
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-27 04:45:56 +0900

    [add]MeshのUtilityを追加

---
+++ Assets/Script/Runtime/Ingame/MeshUtil.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class MeshUtil
+{
+    public static void MeshColliderRefresh(GameObject gameObject)
+    {
+        MeshCollider collider = null;
+
+        if (!gameObject.TryGetComponent(out collider))
+        {
+            collider = gameObject.AddComponent<MeshCollider>();
+        }
+
+        Mesh mesh = gameObject.GetComponent<MeshFilter>().mesh;
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+        collider.sharedMesh = mesh;
+    }
+}
