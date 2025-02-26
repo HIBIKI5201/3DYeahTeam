28d9086b719c7349c8c0d14e2119ec40396c29b9
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-26 11:37:42 +0900

    [add]リザルトUIを追加

---
+++ Assets/Script/Runtime/Ingame/ResultUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ResultUI : MonoBehaviour
+{
+    private UIDocument _document;
+    private void Awake()
+    {
+        _document = GetComponent<UIDocument>();
+    }
+}
