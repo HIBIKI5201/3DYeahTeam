0b3d35283b39ddf87fc2f265688a2f1dc57f119a
HiguchiHaruya <tongkouyangye@gmail.com>
2025-02-26 10:18:01 +0900

    [Update]カット後のオブジェクトが切れなくなるバグ修正

--- Assets/Script/Higuchi/CutterMoveController.cs
+++ Assets/Script/Higuchi/CutterMoveController.cs
@@ -2,6 +2,8 @@
 using System;
 using ChargeShot.Runtime.Ingame;
 using SymphonyFrameWork.System;
+using Unity.VisualScripting;
+using System.Collections.Generic;
 /// <summary>
 /// カッターの管理クラス
 /// </summary>
@@ -11,21 +13,24 @@ public class CutterMoveController : MonoBehaviour
     public float _speed = 2.0f; // 移動速度
     public float _maxPosition = 1.0f; // 右端
     public float _minPosition = -1.0f; // 左端
-    [SerializeField, Header("きゅうり情報")] private GameObject _targetObject;
-    [SerializeField] Material _capMaterial;
+    [SerializeField, Header("きゅうり情報")] Material _capMaterial;
     private float _currentPosition;
-    [SerializeField, Header("ベストタイミング1")] float _bestTiming1;
-    [SerializeField, Header("ベストタイミング2")] float _bestTiming2;
-
+    [SerializeField, Header("ベストタイミング")] float[] _bestTimings = new float[2];
     private Vector3 _cuttingObjectPosition;
     private bool _movingRight = true;
-    private int _cutCount;
+    private int _cutCount = 0;
     private float _distance1;
     private float _distance2;
+    List<float> _distanceList = new List<float>();
+    private GameObject _targetObject;
     private void Update()
     {
-        MoveGauge();
+        MoveCutter();
         CuttingObject();
+    }
+    public void GetTargetObject(GameObject target)
+    {
+        _targetObject = target;
     }
     /// <summary>
     /// 
@@ -42,11 +47,12 @@ public class CutterMoveController : MonoBehaviour
             {
                 var leftSide = pieces[0];
                 var rightSide = pieces[1];
-
+                rightSide.AddComponent<BoxCollider>();
                 // 右側のオブジェクトを少し移動
                 rightSide.transform.position += rightSide.transform.up * 0.5f;
+                _distanceList.Add(Mathf.Abs(_bestTimings[_cutCount] - _currentPosition));
                 _cutCount++;
-                if (_cutCount >= 2)
+                if (_cutCount > 1)
                 {
                     SendIngameSystem();
                 }
@@ -60,16 +66,20 @@ public class CutterMoveController : MonoBehaviour
 
     private void SendIngameSystem()
     {
-        var _distance1 = Mathf.Abs(_currentPosition - _bestTiming1);
-        var _distance2 = Mathf.Abs(_currentPosition - _bestTiming2);
+        float _distance = 0;
+        foreach (var data in _distanceList)
+        {
+            _distance += data;
+        }
         var inGameSystem = ServiceLocator.GetInstance<IngameSystem>();
-        inGameSystem.CucumberData.Phase1Data = _distance1 + _distance2; //差分の合計値を一旦入れとく
+        inGameSystem.CucumberData.Phase1Data = _distance; //差分の合計値を一旦入れとく
+        Debug.Log($"差{_distance}");
     }
 
     /// <summary>
-    /// 針を左右に移動させる
+    /// カッターを左右に移動させる
     /// </summary>
-    private void MoveGauge()
+    private void MoveCutter()
     {
         _cuttingObjectPosition.z = _currentPosition;
         transform.position = _cuttingObjectPosition;
@@ -86,4 +96,5 @@ public class CutterMoveController : MonoBehaviour
     /// 現在の位置を取得
     /// </summary>
     public float GetCurrentPosition() => _currentPosition;
+    private void OnTriggerEnter(Collider other) => _targetObject = other.gameObject;
 }
