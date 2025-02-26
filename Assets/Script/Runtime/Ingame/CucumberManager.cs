c0399aba716b65dbc1871709c69e016e43aeb8db
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-26 15:59:26 +0900

    [hotfix]試しにキュウリのマネージャーを生成するように

---
+++ Assets/Script/Runtime/Ingame/CucumberManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CucumberManager : MonoBehaviour
+{
+    private GameObject _model;
+
+    public GameObject CucumberModel { get => _model; }
+
+    public void Init(GameObject obj)
+    {
+        _model = obj;
+
+        transform.position = obj.transform.position;
+        obj.transform.parent = transform;
+    }
+}
