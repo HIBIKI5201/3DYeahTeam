fa20f3560ffbb34bad7855cf474e146a7c4b00e0
Oriku0308 <O.riku8863@gmail.com>
2025-03-03 03:16:48 +0900

    <Add>  ファイル構造をかえた きゅうりが綺麗に回転するようになった

---
+++ Assets/Script/Runtime/Ingame/Phase2/RotateCucumber.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using SymphonyFrameWork.System;
+using SymphonyFrameWork.Utility;
+
+public class RotateCucumber : MonoBehaviour
+{
+    [SerializeField] private float _speed = 45f; // 回転速度
+
+    private AudioManager _audioManager;
+    private IngameSystem _ingameSystem;
+
+    private GameObject _cucumber;
+    private Renderer _renderer;
+
+    private Vector3 _center; // モデルの中心座標
+
+    private async void Awake()
+    {
+        await Awaitable.NextFrameAsync();
+
+        _audioManager = ServiceLocator.GetInstance<AudioManager>();
+        _ingameSystem = ServiceLocator.GetInstance<IngameSystem>();
+
+        if (_ingameSystem != null)
+        {
+            await SymphonyTask.WaitUntil(() => _ingameSystem.Cucumber, destroyCancellationToken);
+            _cucumber = _ingameSystem.Cucumber.CucumberModel;
+            _renderer = _cucumber.GetComponent<Renderer>();
+        }
+
+        // きゅうりのモデル中心を取得（最初の値を固定）
+        _center = _renderer.bounds.center;
+    }
+
+    private void Update()
+    {
+        // モデルの中心を基準にX軸回転
+        _cucumber.transform.RotateAround(_center, Vector3.right, _speed * Time.deltaTime);
+    }
+}
